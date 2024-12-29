8660e879592d0f8daac88e27a82885be8922a52c
Wil Simpson <wil@forever.dev>
2024-12-29 11:32:26 -0600

    feat: use submodule for proto api

--- pkg/model/game/map.go
+++ pkg/model/game/map.go
@@ -30,7 +30,7 @@ var (
 type Map struct {
 	Id        uuid.UUID  `db:"id" json:"id"`
 	Name      string     `db:"name" json:"name"`
-	MapPath   string     `db:"map_path" json:"map_path"`
+	MapPath   string     `db:"map_path" json:"mapPath"`
 	CreatedAt time.Time  `db:"created_at" json:"createdAt"`
 	UpdatedAt time.Time  `db:"updated_at" json:"updatedAt"`
 	DeletedAt *time.Time `db:"deleted_at" json:"deletedAt"`
